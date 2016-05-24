9105b5264f21344fc0fb06d47dd189dc77a2be8b
Ian Newborn <Ian Newborn>
2016-05-24 12:19:57 +0200

    Changed grid to take a colour when rendered. Mved grid ownership to overlay.

--- grid.py
+++ grid.py
@@ -1,17 +1,16 @@
 import pygame
 
 class grid:
-    def __init__(self, width, height, step, colour):
-        self.colour = colour
+    def __init__(self, width, height, step):
         self.width = width
         self.height = height
         self.step = step
         self.steps = int(self.width/self.step)
         
-    def draw(self, surface):
+    def draw(self, surface, colour):
         pos = 0;
         
         for i in range(0, self.steps):
             pos += self.step
-            pygame.draw.line(surface, self.colour, (0, pos), (self.width, pos))
-            pygame.draw.line(surface, self.colour, (pos, 0), (pos, self.height))        
+            pygame.draw.line(surface, colour, (0, pos), (self.width, pos))
+            pygame.draw.line(surface, colour, (pos, 0), (pos, self.height))        
--- overlay.py
+++ overlay.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame.freetype as freetype
 import random
+import grid
 
 class overlay:
     def __init__(self, width, height):
@@ -17,9 +18,13 @@ class overlay:
         self.height = height
         self.updatePosText()
         freetype.init()
-        self.font = freetype.SysFont("Ariel", 72)
+        self.font = freetype.SysFont("Ariel", 24)
+        self.grid = grid.grid(self.width, self.height, 250)
+        pass
 
     def draw(self, surface):
+        self.grid.draw(surface, self.colour);
+
         pygame.draw.line(surface, self.colour, (0, self.pos[1]), (self.width, self.pos[1]))
         pygame.draw.line(surface, self.colour, (self.pos[0], 0), (self.pos[0], self.height))
         pygame.draw.circle(surface, self.colour, self.pos, 20, 1)
@@ -27,7 +32,9 @@ class overlay:
         pygame.draw.line(surface, self.colour, (self.pos[0] + 10, self.pos[1] - 10), (self.pos[0] + 18, self.pos[1] - 18))
         pygame.draw.line(surface, self.colour, (self.pos[0] + 10, self.pos[1] + 10), (self.pos[0] + 18, self.pos[1] + 18))
         pygame.draw.line(surface, self.colour, (self.pos[0] - 10, self.pos[1] + 10), (self.pos[0] - 18, self.pos[1] + 18))
-        self.font.render_to(surface, (0, 0), self.posText, self.colour, None, 0, 0, 72)
+        
+        self.font.render_to(surface, (0, 0), self.posText, self.colour, None, 0, 0, 24)
+        pass
 
     def updatePosText(self):
         self.posText = ''.join((str(self.pos[0]/10.0), ', ', str(self.pos[1]/10.0)))
@@ -61,7 +68,6 @@ class overlay:
                 yPos = self.startPos[1] + ((self.targetPos[1] - self.startPos[1]) * timeOffset)
                 self.colour = self.tweenColours(self.startColour, self.targetColour, timeOffset)
                 self.pos = (int(round(xPos)), int(round(yPos)))
-        self.updatePosText()
         pass
 
     def targetReached(self, updateTime):
@@ -72,6 +78,8 @@ class overlay:
         self.moveTime = random.randrange(2, 5)
         self.startColour = self.targetColour
         self.targetColour = self.pickNewColour()
+        self.updatePosText()
+        pass
 
     def tweenColours(self, startColour, endColour, progress):
         return (int(round((startColour[0] + ((endColour[0] - startColour[0]) * progress)))),int(round((startColour[1] + ((endColour[1] - startColour[1]) * progress)))),int(round((startColour[2] + ((endColour[2] - startColour[2]) * progress)))))
