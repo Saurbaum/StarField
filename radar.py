cd7aa1eb5dfa389192e03237e0ac2974ba076631
Unknown <saurbaum@gmail.com>
2017-03-19 11:58:39 +0000

    Radar improvements
    Optimised drawing over the overlay to render once to its own surface once on startup.
    Added Major and Minor ticks to the radar display.

--- radar.py
+++ radar.py
@@ -1,5 +1,4 @@
 import sys
-import time
 import math
 from collections import deque
 import pygame
@@ -18,7 +17,7 @@ class radar:
 
         self.colour = (0,128,0)
 
-        self.startTime = time.time()
+        self.startTime = 0
         self.moveTime = 5
 
         self.rotationSpeed = 360.0
@@ -32,13 +31,41 @@ class radar:
         self.armPoint = (self.centre[0], self.centre[1] - self.armLength)
         self.startPoint = self.armPoint
 
-        self.now = time.time()
+        self.majorTicks = deque()
+        self.minorTicks = deque()
+
+        self.overLaySurface = pygame.Surface((width, height), pygame.HWSURFACE)
+        self.prepareOverlay()
+
+    def prepareOverlay(self):
+        self.majorTickReference = ((self.centre[0], self.centre[1] + (self.radius + 6)),(self.centre[0], self.centre[1] + (self.radius - 6)))
+        majorTickAngle = 45
+        for i in range(0, 360//majorTickAngle):
+            self.createTicks(self.majorTickReference, self.majorTicks, i*majorTickAngle)
+
+        self.minorTickReference = ((self.centre[0], self.centre[1] + (self.radius + 4)),(self.centre[0], self.centre[1] + (self.radius - 4)))
+        minorTickAngle = 11.25
+        for i in range(0, int(360/minorTickAngle)):
+            self.createTicks(self.minorTickReference, self.minorTicks, i*minorTickAngle)
+
+        pygame.draw.circle(self.overLaySurface, self.colour, self.centre, self.radius, 3)
+        for tick in self.majorTicks:
+            pygame.draw.line(self.overLaySurface, self.colour, tick[0], tick[1], 3)
+
+        for tick in self.minorTicks:
+            pygame.draw.line(self.overLaySurface, self.colour, tick[0], tick[1], 1)
+
+
+    def createTicks(self, referencePos, ticks, angle):
+        first = self.rotatePoint(self.centre, angle, referencePos[0])
+        second = self.rotatePoint(self.centre, angle, referencePos[1])
+        ticks.append((first, second))
 
     def drawBackground(self, surface):
-        surface.fill((0,32,0))
+        surface.fill((0,28,0))
 
     def drawOverlay(self, surface):
-        pygame.draw.circle(surface, self.colour, self.centre, self.radius, 3)
+        surface.blit(self.overLaySurface, (0,0))
         pygame.draw.line(surface, self.colour, self.centre, self.armPoint, 3)
 
     def on_loop(self, updateTime):
