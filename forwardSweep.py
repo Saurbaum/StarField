40baeada3a3ea52f0363d95e053e96c320cef50c
Unknown <saurbaum@gmail.com>
2017-03-26 14:58:16 +0100

    Forward sweep
    Added start of overlay

--- forwardSweep.py
+++ forwardSweep.py
@@ -2,6 +2,7 @@ import math
 from collections import deque
 import pygame
 from pygame.locals import *
+from rotation import *
 
 class forwardSweep:   
     def __init__(self, width, height, displaySurface):
@@ -9,7 +10,7 @@ class forwardSweep:
 
         self.centre = (width//2, height//2)
 
-        maxSpace = min(width, height)
+        self.maxSpace = min(width, height)
 
         self.colour = (0,128,0)
 
@@ -19,18 +20,29 @@ class forwardSweep:
         self.currentAngle = self.startAngle
         self.rotationSpeed = 1
 
-        self.radius = int(maxSpace * 0.95)
+        self.radius = int(self.maxSpace * 0.95)
         self.armLength = self.radius
         
         self.armStartPoint = (self.centre[0], int(height*0.98)) 
         self.armPoint = (self.centre[0], self.armStartPoint[1] - self.armLength)
         self.startPoint = self.armPoint
 
+        self.maxAngle = 0;
+
         self.overLaySurface = pygame.Surface((width, height), pygame.HWSURFACE)
         self.prepareOverlay()
 
     def prepareOverlay(self):
-        self.overLaySurface.fill((0,0,0))
+        width = self.centre[0]*2
+
+        for angle in range(0,90):
+            testPoint = rotatePoint(self.armStartPoint, angle, self.armPoint)
+            if testPoint[0] < (width * 0.99) :
+                self.maxAngle = angle
+
+        pygame.draw.line(self.overLaySurface, self.colour, self.armStartPoint, rotatePoint(self.armStartPoint, self.maxAngle, self.armPoint), 1)
+        pygame.draw.line(self.overLaySurface, self.colour, self.armStartPoint, rotatePoint(self.armStartPoint, -self.maxAngle, self.armPoint), 1)
+
 
     def on_loop(self, updateTime):
         self.now += updateTime
@@ -57,7 +69,7 @@ class forwardSweep:
         surface.fill((128,128,0))
 
     def drawOverlay(self, surface):
-        #surface.blit(self.overLaySurface, (0,0))
+        surface.blit(self.overLaySurface, (0,0))
         pygame.draw.line(surface, self.colour, self.armStartPoint, self.armPoint, 3)
 
 
